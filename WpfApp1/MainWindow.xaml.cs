e7820985f99d137ba58ecc50e66dc379310e2b39
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2023-11-07 14:44:23 +0100

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       List<Organization> szervezetek = new List<Organization> ();
+    private void Betoltes(string filename)
+        {
+            foreach(var sor in File.ReadAllLines(filename).Skip(1))
+            {
+                szervezetek.Add(new Organization(sor.Split(';')));
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Betoltes("organizations-100000.csv");
+            dgAdatok.ItemsSource = szervezetek;
+
+            //cbOrszag.ItemsSource = szervezetek.Select(x => x.Country).OrderBy(x=>x).Distinct().ToList();
+            //cbEv.ItemsSource = szervezetek.Select(x=>x.Founded).OrderBy(x=>x).Distinct().ToList();
+            //labTotalEmpl.Content = szervezetek.Sum(x => x.EmployeesNumber);
+        }
+    }
+}
